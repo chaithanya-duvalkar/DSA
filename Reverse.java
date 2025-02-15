b93d2523fdb4194ae3f5e4104858d3c8551104e0
Chaithanya S <chaithanya9ig@gmail.com>
2025-02-15 21:20:16 +0530

    final commit

---
+++ Reverse.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Reverse {
+    public static void reverse(int[] array,int n)
+    {
+        int start=0,end=n-1;
+        while(start<end)
+        {
+            int temp=array[end];
+            array[end]=array[start];
+            array[start]=temp;
+
+            start++;
+            end--;
+        }
+        }
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("enter n");
+        n=sc.nextInt();
+        int[] array=new int[n];
+        System.out.println("enter array");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        }
+
+        reverse(array,n);
+
+        System.out.println("after reversing");
+        for(int i=0;i<n;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+} 
