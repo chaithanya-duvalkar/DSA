b14268284ca7c871b887bbc3e8e73599146a8730
Chaithanya S <chaithanya9ig@gmail.com>
2025-02-15 17:08:31 +0530

    updated commit

--- pgm2.java
+++ pgm2.java
@@ -18,12 +18,84 @@ public class pgm2{
         int b=5;
         System.out.println(a);
         System.out.println(b);
-        String data="chaithej<3<3<34EVER";
+        String data="chai";
         System.out.println(data);
         a=9;
         System.out.println(a);
     }
 }    
     */
+
+
+    //arithmetic operators
+    //binary operators-->+,-,*,/,%
+    //relational operators-->==,!=,>,<,<=,>=
+    //logical operators-->&&,||,!
+    //Assignment operators-->=,+=,-=,*=,/=
+    //unary
+    public class pgm2{
+        public static void main(String args[]){
+        /*
+        //pre-increment
+         int a=1;
+         int b=++a;   //first increments the value of a and stores in a and then in b
+         System.out.println("a="+a);
+         System.out.print("b="+b);
+         */
+        
+        /* 
+        //post-increment
+        int a=1;
+        int b=a++;  //here a value is assigned to b then a is incremented to 1 and stored in it
+        System.out.println("a="+a);
+        System.out.print("b="+b);
+
+        */
+
+        /* 
+        //post-decrement
+        int a=5;
+        int b=a--;
+        System.out.println("a="+a);
+        System.out.print("b="+b);
+
+        */
+
+        /*
+        //pre-decrement
+         int a=5;
+         int b=--a;
+         System.out.println("a="+a);
+         System.out.print("b="+b);
+      
+          */
+
+          /* 
+          int A=10;
+          int B=10;
+          System.out.println((A>=B));
+
+          */
+
+          /* 
+          System.out.println((3>2)&&(5<0));
+          System.out.println((3>2)||(5<0));
+          System.out.println(!(3>2));
+          */
+
+          int x=10;
+          int y=5;
+          x+=1;
+          y-=2;
+          System.out.println(x);
+          System.out.println(y);
+          x*=2;
+          y/=3;
+          System.out.println(x);
+          System.out.println(y);
+
+        }
+    }
+
 
     
