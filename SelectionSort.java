11120a95b83a51840510e0c7bb077a809719c579
Chaithanya S <chaithanya9ig@gmail.com>
2025-02-18 13:35:48 +0530

    final commit

---
+++ SelectionSort.java
@@ -0,0 +1,55 @@
+//time complexity-->O(n^2)
+//in selection sort the smallest element is picked and sent to the start index of the array
+//the first element is considered as smallest element and compared with next elements
+//dividing the array as sorted part and unsorted part
+
+import java.util.*;
+
+public class SelectionSort {
+    
+    public static void selectionsort(int[] array,int n){
+        int swap=0;
+
+        for(int i=0;i<n-1;i++){
+            int minPos=i;  //storing index not the value
+            for(int j=i+1;j<n;j++){
+                //loop
+                if(array[minPos]>array[j]){
+                    minPos=j;
+                }
+            }
+            //swap
+            if(minPos!=i){
+                int temp=array[minPos];
+                array[minPos]=array[i];
+                array[i]=temp;
+                swap++;
+            }
+          
+        }
+       System.out.println("no. of swap="+swap);
+    }
+
+    public static void printarray(int[] array,int n){
+        for(int i=0;i<n;i++){
+            System.out.print(array[i]+" ");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n;
+        System.out.println("enter n");
+        n=sc.nextInt();
+        int[] array=new int[n];
+        System.out.println("enter array");
+        for(int i=0;i<n;i++){
+            array[i]=sc.nextInt();
+        } 
+
+        selectionsort(array,n);
+        printarray(array,n);
+        
+    }
+}
+
