8095a61c8dd14fa7a9650b7c2ae1e66dc8a8254d
Chaithanya S <chaithanya9ig@gmail.com>
2025-02-21 10:38:05 +0530

    final commit

---
+++ DiagonalMatrix.java
@@ -0,0 +1,76 @@
+import java.util.*;
+
+public class DiagonalMatrix 
+{
+
+    public static int diagonalmatrix(int[][] matrix,int n)
+    {
+         int sum=0;
+
+         /* 
+         for(int i=0;i<n;i++)        //time complexity--->O(n^2)
+         {
+            for(int j=0;j<m;j++)
+            {
+                if(i==j)
+                {
+                    sum+=matrix[i][j];
+                }
+                if(i+j==n-1)
+                {
+                    sum+=matrix[i][j];
+                }
+
+            }
+         }
+         return sum;
+         */
+
+         for(int i=0;i<n;i++)      //time complexity--->O(n)
+        {
+            //primary diagonal
+            sum+=matrix[i][i];
+
+            //secondary diagonal
+            if(i!=n-1-i){
+                sum+=matrix[i][n-i-1];
+            }
+        }
+        return sum;
+
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter n");
+        int n=sc.nextInt();
+        System.out.println("enter m");
+        int m=sc.nextInt();
+        
+        int[][] matrix =new int[n][m];
+
+        System.out.println("enter elements of matrix:");
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                matrix[i][j]=sc.nextInt();
+            }
+        }
+
+        System.out.println("matrix are:");
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+            System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int sum=diagonalmatrix(matrix,n);
+        System.out.println("diagonal matrix sum:"+sum); 
+        
+
+}
+}
